317edc8bf51fa3e0c21e94d661d4ffebe2383cd5
sanaullah <su243445@gmail.com>
2024-10-21 23:27:04 +0500

    json parser and middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import productsRoutes from "./routes/products/index";
 
 const port = 3000;
 const app = express();
-
+app.use(urlencoded({ extended: false }));
+app.use(json());
 app.get("/", (req, res) => {
   res.send("hello world");
 });
--- src/routes/products/productController.ts
+++ src/routes/products/productController.ts
@@ -8,6 +8,7 @@ export function getProductById(req: Request, res: Response) {
 }
 
 export function createProduct(req: Request, res: Response) {
+    console.log(req.body);
   res.send("createProduct");
 }
 export function updateProduct(req: Request, res: Response) {
