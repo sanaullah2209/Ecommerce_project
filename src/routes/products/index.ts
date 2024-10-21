38f9cdd5625d5031191b03c26f688d57a84687ab
sanaullah <su243445@gmail.com>
2024-10-21 23:06:46 +0500

    Product router and controller

--- src/routes/products/index.ts
+++ src/routes/products/index.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
+import {
+  listProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "./ProductController";
 // endpoints products
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("the list of product");
-});
-router.get("/:id", (req, res) => {
-  console.log(req.params);
-  res.send("A product");
-});
-router.post("/", (req, res) => {
-  res.send("New products created");
-});
+router.get("/", listProducts);
+router.get("/:id", getProductById);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
---
+++ src/routes/products/productController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+export function listProducts(req: Request, res: Response) {
+  res.send("listProducts");
+}
+
+export function getProductById(req: Request, res: Response) {
+  res.send("getProductsbyId");
+}
+
+export function createProduct(req: Request, res: Response) {
+  res.send("createProduct");
+}
+export function updateProduct(req: Request, res: Response) {
+  res.send("updateProduct");
+}
+export function deleteProduct(req: Request, res: Response) {
+  res.send("deleteProduct");
+}
