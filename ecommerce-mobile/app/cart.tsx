e93ef34f8e6d37081220c7db3de5907a93af80cc
sanaullah <sanaullah2209f@aptechgdn.net>
2024-12-16 21:44:29 +0500

    struture are done of mobile applictiaon

---
+++ ecommerce-mobile/app/cart.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function CartPage(){
+    return(
+        <View>
+            <Text>Cart Page</Text>
+            {/* Add your cart items here */}
+        </View>
+    )
+}
---
+++ ecommerce-mobile/app/index.tsx
@@ -0,0 +1,15 @@
+import { FlatList, Text, View } from "react-native";
+import products from "../assets/products.json"
+import ProductListItem from "../components/ProductListItem";
+
+
+
+
+export default function HomeScreen(){
+    return(
+        <View>
+            <FlatList data={products} renderItem={({item})=><ProductListItem product={item}/>}/>
+           
+        </View>
+    )
+}
